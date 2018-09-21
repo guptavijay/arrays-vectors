b01a10c75ad280b3eb5875c2be5fa6ed314da316
Vijay Kumar Gupta <vg7579@gmail.com>
2018-09-21 17:03:00 +0530

    Create squares_chess_boost.cpp

---
+++ squares_chess_boost.cpp
@@ -0,0 +1,26 @@
+//boost library
+
+#include <iostream>
+#include<bits/stdc++.h>
+#include<boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    cpp_int x=0;
+
+	    for(int i=1;i<=n;i++)
+	    {
+	      x=x+ (i*i);
+	    }
+	    cout<<x<<endl;
+	}
+	return 0;
+}
